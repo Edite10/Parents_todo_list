7b0073809016c3901d6420970084a2b6f2b93064
fouziaa62 <fouziaahmad62@gmail.com>
2025-03-27 12:47:59 +0000

    authentication done

--- parents_todo/urls.py
+++ parents_todo/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from todo import views
+from django.views.generic import TemplateView
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
+    path('home', views.home, name='home'),
     path("admin/", admin.site.urls),
+    path('todo', include('todo.urls')),   # Include todo app's URLs
+    path('templates/', include('django.contrib.auth.urls')),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+
+def landing(request):
+    return render(request, "todo/landing.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # redirect to the home page after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout(request):
+    return render(request, "todo/logout.html")  
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after signup
+            return redirect("home")  # Redirect to homepage after signup
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
+
+
+def home(request):
+    return render(request, "todo/home.html")
