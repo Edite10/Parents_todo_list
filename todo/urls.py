460b5b50607ed26fd929ba00c4c9ca021a5df54a
mathew <m@ringingtree.co.uk>
2025-03-27 14:36:20 +0000

    Added: Update Function and associated files

--- todo/urls.py
+++ todo/urls.py
@@ -3,7 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name="todo/login.html"), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name="todo/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
@@ -15,5 +16,7 @@ urlpatterns = [
     path("create/",
          views.create_todo,
          name='create_todo'),
+    path("update/<int:task_id>/",
+         views.update_todo,
+         name="update_todo"),
 ]
-
