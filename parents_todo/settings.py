17085d84a880f485b42767c873067b247c187976
joshwill234 <234joshwill@gmail.com>
2025-03-28 13:05:09 +0000

    database settings updated

--- parents_todo/settings.py
+++ parents_todo/settings.py
@@ -84,10 +84,9 @@ WSGI_APPLICATION = "parents_todo.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}'
-    )
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 CSRF_TRUSTED_ORIGINS = [
